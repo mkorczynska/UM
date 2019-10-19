c9b0f7b753fbaaca3304fefb9b279d08520d8836
mkorczynska <k.martyna95@gmail.com>
2019-10-19 16:36:13 +0200

    .

--- boston.py
+++ boston.py
@@ -1,33 +1,98 @@
-import pandas
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import sklearn
-import math
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import RidgeCV, LassoCV, Ridge, Lasso
+# from sklearn.feature_selectionimport VarianceThreshold
+from sklearn import metrics
+
 from sklearn.datasets import load_boston
 
-dataset = load_boston()
-print(dataset.keys())
-print(dataset.feature_names)
+boston = load_boston()
+print(boston.data.shape)
+print(boston)
 
-df = pandas.DataFrame(dataset.data)
+x = load_boston()
+df = pd.DataFrame(x.data, columns=x.feature_names)
+df["MEDV"] = x.target
+X = df.drop("MEDV", 1)
+y = df["MEDV"]
+df.head()
 print(df.head())
 
-df.columns = dataset.feature_names
-print(df.head())
+plt.figure(figsize=(12, 10))
+cor = df.corr()
+sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
+# plt.show()
 
-df['PRICE'] = dataset.target
-print(df.describe())
 
-X = df.drop('PRICE', axis=1)
-Y = df['PRICE']
+# Correlation with output variable
+cor_target = abs(cor["MEDV"])
+# Selecting highly correlated features
+relevant_features = cor_target[cor_target > 0.5]
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
+print(df[["LSTAT", "PTRATIO"]].corr())
+print(df[["RM", "LSTAT"]].corr())
 
-regressor = LinearRegression()
-regressor.fit(X_train, Y_train)
-print(regressor.coef_)
-print(regressor.intercept_)
-print('R2 train', regressor.score(X_train, Y_train))
-print('R2 test', regressor.score(X_test, Y_test))
+# print(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+boston_lr = LinearRegression()
+boston_lr.fit(X_train, y_train)
+print(boston_lr.coef_)
+print(boston_lr.intercept_)
+print('R2 for train: ', boston_lr.score(X_train, y_train))
+print('R2 for test: ', boston_lr.score(X_test, y_test))
+
+y_pred = boston_lr.predict(X_test)
+df = pd.DataFrame({'actual': y_test, 'pred': y_pred})
+print(df)
+
+print(pd.DataFrame(boston_lr.coef_))
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+# -------------------------------------
+boston_rr = Ridge()
+boston_rr.fit(X_train, y_train)
+print(boston_rr.coef_)
+print(boston_rr.intercept_)
+print('R2 for train: ', boston_rr.score(X_train, y_train))
+print('R2 for test: ', boston_rr.score(X_test, y_test))
+
+y_pred = boston_rr.predict(X_test)
+df = pd.DataFrame({'actual': y_test, 'pred': y_pred})
+print(df)
+
+print(pd.DataFrame(boston_rr.coef_))
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+# -------------------------------------
+boston_l = Lasso()
+boston_l.fit(X_train, y_train)
+print(boston_l.coef_)
+print(boston_l.intercept_)
+print('R2 for train: ', boston_l.score(X_train, y_train))
+print('R2 for test: ', boston_l.score(X_test, y_test))
+
+y_pred = boston_l.predict(X_test)
+df = pd.DataFrame({'actual': y_test, 'pred': y_pred})
+print(df)
+
+print(pd.DataFrame(boston_l.coef_))
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+pd.Series(boston_l.coef_)
+plt.show()
