6ac8b71a67bed11b1969a93d752d274864359cfc
mkorczynska <k.martyna95@gmail.com>
2019-12-08 21:07:03 +0100

    .

--- titanic-cw4.py
+++ titanic-cw4.py
@@ -2,37 +2,30 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cluster import KMeans
 
 pd.options.display.max_columns = 999
 data = pd.read_csv('train.csv')
-print(data)
-print(data.head())
-print(data.describe())
-print(data.info())
+# print("Data\n", data)
+print("Data head\n", data.head())
+print("Data describe\n", data.describe())
 
-# df = pd.DataFrame(data)
-# cor = df.corr()
-# sns.heatmap(cor, annot=True)
-# plt.show()
-
-from sklearn.preprocessing import LabelEncoder
-
-labelEncoder = LabelEncoder()
-labelEncoder.fit(data['Sex'])
-data['Sex'] = labelEncoder.transform(data['Sex'])
-y = data['Survived']
-data = data.drop(['Name', 'Ticket', 'Cabin', 'Embarked', 'Survived'], axis=1)
-data = data.fillna(data.mean())
-from sklearn.cluster import KMeans
-
-kmeans = KMeans(n_clusters=2)
-kmeans.fit(data)
-correct = 0
-X = np.array(data).astype(float)
-for i in range(len(X)):
-    pred = np.array(X[i].astype(float))
-    pred = pred.reshape(-1, len(pred))
-    pred = kmeans.predict(pred)
-    if (pred == y[i]):
-        correct += 1
-print(correct / len(X))
+# labelEncoder = LabelEncoder()
+# labelEncoder.fit(data['Sex'])
+# data['Sex'] = labelEncoder.transform(data['Sex'])
+# y = data['Survived']
+# data = data.drop(['Name', 'Ticket', 'Cabin', 'Embarked', 'Survived'], axis=1)
+# data = data.fillna(data.mean())
+#
+# kmeans = KMeans(n_clusters=2)
+# kmeans.fit(data)
+# correct = 0
+# X = np.array(data).astype(float)
+# for i in range(len(X)):
+#     pred = np.array(X[i].astype(float))
+#     pred = pred.reshape(-1, len(pred))
+#     pred = kmeans.predict(pred)
+#     if (pred == y[i]):
+#         correct += 1
+# print(correct / len(X))
