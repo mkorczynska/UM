1bceef2fd30f5e396e304032cda038d6fc7197f8
mkorczynska <k.martyna95@gmail.com>
2019-12-15 23:25:35 +0100

    .

--- titanic-cw4.py
+++ titanic-cw4.py
@@ -4,33 +4,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.cluster import KMeans
-import re
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
-from sklearn.decomposition import PCA as sklearnPCA
 
 pd.options.display.max_columns = 999
 data = pd.read_csv('train.csv')
 
-# g0 = sns.FacetGrid(data, col='Survived')
-# g0.map(plt.hist, 'Age', bins=20)
-# g1 = sns.FacetGrid(data, col='Survived')
-# g1.map(plt.hist, 'Fare', bins=20)
-# g2 = sns.FacetGrid(data, col='Survived', row='Pclass')
-# g2.map(plt.hist, 'Age', bins=20)
-# g3 = sns.FacetGrid(data, col='Survived')
-# g3.map(plt.hist, 'Age', bins=20)
-# g4 = sns.FacetGrid(data, col='Survived', row='Embarked')
-# g4.map(plt.hist, 'Age', bins=20)
-# plt.show()
+g0 = sns.FacetGrid(data, col='Survived')
+g0.map(plt.hist, 'Age', bins=20)
+g1 = sns.FacetGrid(data, col='Survived')
+g1.map(plt.hist, 'Fare', bins=20)
+g2 = sns.FacetGrid(data, col='Survived', row='Pclass')
+g2.map(plt.hist, 'Age', bins=20)
+g3 = sns.FacetGrid(data, col='Survived')
+g3.map(plt.hist, 'Age', bins=20)
+g4 = sns.FacetGrid(data, col='Survived', row='Embarked')
+g4.map(plt.hist, 'Age', bins=20)
+plt.show()
 
-# print("Data\n", data)
 print("Data head\n", data.head())
 print("Data describe\n", data.describe())
 
@@ -55,8 +46,19 @@ for i in range(len(X)):
 print("Correctly classified: ", correct)
 print("Correctly classified %: ", correct / len(X))
 
-data['Survived'] = y
-data = data.drop(['PassengerId'], axis=1)
-print(data.corr())
 # -----------------------------------------
+data = pd.read_csv('train.csv')
+labelEncoder = LabelEncoder()
+labelEncoder.fit(data['Sex'])
+data['Sex'] = labelEncoder.transform(data['Sex'])
+y = data['Survived']
+data = data.drop(['Name', 'Ticket', 'Cabin', 'Embarked', 'Survived'], axis=1)
+data = data.fillna(data.mean())
+X = data
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train)
+X_test_pca = pca.transform(X_test)
+print('Original number of features:', X_train.shape[1])
+print('Reduced number of features:', X_train_pca.shape[1])
